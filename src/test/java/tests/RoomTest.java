080486418c5e3dbd61933707a99a1551931e7c1a
phuchh <phuchh@inmobivn.com>
2026-04-07 09:48:00 +0700

    feat: Add Auto-Login capability to RoomTest if triggered by Guest wall

--- src/test/java/tests/RoomTest.java
+++ src/test/java/tests/RoomTest.java
@@ -9,6 +9,10 @@ import pages.tructuyen.tabtructuyen.TrucTuyenScr;
 import pages.tructuyen.tabtructuyen.phong.TaoPhongScr;
 import pages.tructuyen.tabtructuyen.phong.CuaToiScr;
 import pages.tructuyen.tabtructuyen.phong.PhongScr;
+import pages.toi.ToiGuestScr;
+import pages.toi.login.AccountScr;
+import pages.toi.login.LoginMethodScr;
+import pages.toi.login.LoginUIDScr;
 import utils.StepUtils;
 
 public class RoomTest extends BaseDriver {
@@ -24,7 +28,28 @@ public class RoomTest extends BaseDriver {
             () -> Assert.assertTrue(trucTuyenScr.isLoaded(), "Không ở tab Trực tuyến")
         );
 
-        TaoPhongScr taoPhongScr = StepUtils.step("Mở màn hình Tạo phòng", trucTuyenScr::clickTaoPhong);
+        TaoPhongScr taoPhongScr = StepUtils.step("Mở màn hình Tạo phòng", () -> {
+            trucTuyenScr.clickTaoPhong();
+            ToiGuestScr guestPage = new ToiGuestScr(driver);
+            if (guestPage.isGuestPageDisplayed()) {
+                System.out.println("[RoomTest] Bấm Tạo Phòng yêu cầu Đăng Nhập. Bắt đầu luồng đăng nhập tự động...");
+                base.BaseScr pageAfterClick = guestPage.clickLogin();
+                LoginMethodScr methodPage;
+                if (pageAfterClick instanceof AccountScr accountScr) {
+                    accountScr.selectAnotherMethodLogin();
+                    LoginMethodScr.waitForLoginMethodScreen(driver);
+                    methodPage = new LoginMethodScr(driver);
+                } else {
+                    methodPage = (LoginMethodScr) pageAfterClick;
+                }
+                LoginUIDScr uidPage = (LoginUIDScr) methodPage.loginWith("uid");
+                uidPage.loginByUID("6026833", "Abcd1234");
+                
+                System.out.println("[RoomTest] Login thành công, bấm Tạo phòng lần 2.");
+                trucTuyenScr.clickTaoPhong();
+            }
+            return new TaoPhongScr(driver);
+        });
 
         String roomName = "ngoc lele bao " + System.currentTimeMillis();
         CuaToiScr roomPage = StepUtils.step("Tạo phòng riêng tư",
